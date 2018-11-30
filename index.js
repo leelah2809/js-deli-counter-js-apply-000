90ce8e0e2e7bb81426fc2f7503ab76a0f5f27517
Okunpls Jelilat Dasola <jelilatokunola2809@gmail.com>
2018-11-30 08:45:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+ 
+ /*{position: [1, 2, 3, 4, 5]
+             names: [ade, josh, faith, eve, maron]};
+             */
+
+let deli = '';
+function takeANumber (currentLine ,personName){
+      var katzDeliLine = [];
+      
+  do {
+    katzDeliLine.push (currentLine);
+    let msg = ('Welcome, ' + personName +  '. You are number ' + currentLine+' in line.');
+    console.log (msg);
+      
+        }
+        
+        while (currentLine < 2);
+       /*while ( currentLine < 3 ){
+         katzDeliLine.push (currentLine);
+          currentLine++;
+            }*/
+    
+  
+}
+
+var currentLine = 1;
+
+  takeANumber(currentLine, 'Ada');
