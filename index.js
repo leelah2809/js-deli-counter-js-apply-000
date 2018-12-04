2d7bfbc5c995df232f9af9b2a77bf0359357b43c
Okunpls Jelilat Dasola <jelilatokunola2809@gmail.com>
2018-12-04 14:34:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(currentLine, name){
+  let katzDeliLine = [];
+  let personName = [];
+  
+  for (let i = 0; i <= name.length; i++ ){
+  currentLine = i;
+     katzDeliLine.push(currentLine);
+     personName. push(name);}
+    
+   
+return ('Welcome, ' +name+ '. You are number ' +currentLine+ ' in line.');
+  
+}
+ takeANumber(currentLine, 'Ada');
