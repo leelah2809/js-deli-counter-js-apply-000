55dd4f3d89e65932b81e05e13a07f4288177842c
Okunpls Jelilat Dasola <jelilatokunola2809@gmail.com>
2018-12-05 18:40:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var katzDeliLine =[];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine. push(name);
+  return ( 'Welcome, ' + name + '. You are number ' + katzDeliLine.length+ ' in line.');
+}
+    
+console.log ( takeANumber(katzDeliLine, 'Ade'));
+     console.log ( takeANumber(katzDeliLine, 'Moji'));
+      console.log ( takeANumber(katzDeliLine, 'Daniel'));
+      
+      
+ function nowServing(katzDeliLine){
+        if (katzDeliLine.length === 0){
+          return ('There is nobody waiting to be served!');
+        }
+        else{var remove = katzDeliLine.shift(0);
+        return ( 'Currently serving ' + remove+ '.' );
+          
+        }
+        
+      }
+
+  console.log(nowServing(katzDeliLine));
+  
+ function currentLine(katzDeliLine){
+        if (katzDeliLine.length === 0){
+          return ('The line is currently empty.');
+        } else { 
+          for ( var i = 0; i <= katzDeliLine.length; i++){
+            //var noArray = katzDeliLine.join ('i.', ', ');
+            var noArray = katzDeliLine.forEach ( i, '. ', katzDeliLine, ', ');  
+            var index = 
+            console.log( 'The line is currently: ' + noArray);
+  
+ 
+         // return( 'The line is currently: ' + katzDeliLine.join ( ' 1., '));          
+        }
+        }
+      }
+   
+   console.log(nowServing (katzDeliLine));
