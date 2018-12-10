23e278401b58b5a1d4693585165aa3253fc91cff
Okunpls Jelilat Dasola <jelilatokunola2809@gmail.com>
2018-12-10 09:34:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+var katzDeliLine =[];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine. push(name);
+  return ( 'Welcome, ' + name + '. You are number ' + katzDeliLine.length+ ' in line.');
+}
+    
+console.log ( takeANumber(katzDeliLine, 'Ade'));
+     console.log ( takeANumber(katzDeliLine, 'Moji'));
+      console.log ( takeANumber(katzDeliLine, 'Daniel'));
+      
+      
+ function nowServing(katzDeliLine){
+        if (katzDeliLine.length === 0){
+          return ('There is nobody waiting to be served!');
+        }
+        else{var remove = katzDeliLine.shift(0);
+        return ( 'Currently serving ' + remove+ '.' );
+          
+        }
+        
+      }
+
+  console.log(nowServing(katzDeliLine));
+  
+ function currentLine(katzDeliLine){
+        if (katzDeliLine.length === 0){
+          return ('The line is currently empty.');
+        } else { 
+          /*for ( var i = 1; i <= katzDeliLine.length; i++){
+         i = katzDeliLine.length;
+          return( 'The line is currently: ' +i+'. ' +katzDeliLine.join ( ' , '));   
+          
+        }*/
+        //let index = ['1.', '2. ', '3. '];
+        katzDeliLine.forEach (function ( index , katzDeliLine)
+        { return ('The line is currently serving' + index+katzDeliLine);
+         }
+        ); 
+      }
+      }
+   
+   console.log(nowServing (katzDeliLine));
